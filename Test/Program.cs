afe3498f9950a087cb33e88a57853ff0266eb843
Vinay Saini <vinay.s@surya-soft.com>
2023-08-27 15:57:00 +0530

    init commit

---
+++ Test/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        System.Console.WriteLine("Program Start....");
+        if (args is null || args.Length == 0)
+            throw new TestFailException("No Argument Passed.");
+        System.Console.WriteLine($"Passed arguement is {string.Join("||=||", args)}");
+        System.Console.WriteLine("Program Complete...");
+    }
+}
+
+public class TestFailException : Exception
+{
+    public TestFailException(string messsage) : base(messsage)
+    {
+        System.Console.Error.WriteLine(messsage);
+    }
+}
